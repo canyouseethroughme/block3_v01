a1300adb0bfc8d859f63a067031ecde776a9df1b
Stefan Andrei <andatud@dfds.com>
2023-02-04 12:40:50 +0100

    Installed apollo client and queried artists

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
+import { useQuery, gql } from "@apollo/client";
+
+const QUERY = gql`
+  {
+    artists {
+      name
+      coverPhoto {
+        url
+      }
+      genre {
+        genre
+      }
+    }
+  }
+`;
 const App = () => {
-  return <div className="App">Hello world!</div>;
+  const { loading, error, data } = useQuery(QUERY);
+
+  if (loading)
+    return (
+      <code>
+        <b>Loading ...</b>
+      </code>
+    );
+
+  if (error)
+    return (
+      <code>
+        <b>Error :/</b>
+      </code>
+    );
+
+  return (
+    <div className="App">
+      <code>
+        <b> Artists:</b> <br />
+      </code>
+      {data &&
+        data.artists.map((artist: any, i: number) => (
+          <code key={i}>
+            {artist.name}
+            <br />
+          </code>
+        ))}
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cldoj93sp14df01uocgyx9qi6/master",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
